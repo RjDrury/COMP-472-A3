f0f886a0ff1dcab43323938caf2d1d230724c122
Claudia <claudia@bliinx.ca>
2020-12-09 16:05:48 -0500

    list of dictionaries fixed

--- app.py
+++ app.py
@@ -8,14 +8,15 @@ def main():
     original_vocab = get_original_vocabulary(index_to_content_map)
     filtered_vocab = get_filtered_vocabulary(index_to_content_map)
 
-    print(len(original_vocab))
-    print(len(filtered_vocab))
+    # print(len(original_vocab))
+    # print(len(filtered_vocab))
 
     original_dict = get_list_of_dictionaries(index_to_content_map, original_vocab)
     filtered_dict = get_list_of_dictionaries(index_to_content_map, filtered_vocab)
 
     # created a model object, needs to be trained and then used to predict
-    # model = nb_bow()
+    model = nb_bow()
+    model.train(filtered_dict, index_to_validity_map)
 
 
 
