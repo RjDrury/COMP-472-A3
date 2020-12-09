a28ecef33880612542dcc7fefd9a6449323c13e1
GwenLegate <gwendolyne180@gmail.com>
2020-12-09 10:31:50 -0500

    added class to create model object and method to train model plus supporting methods.

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 from helpers import *
+from nb_bow import *
 
 
 def main():
-    all_claims, covid_claims, non_covid_claims = get_training_data_claims()
+    #all_claims, covid_claims, non_covid_claims = get_training_data_claims()
+    model = nb_bow()
+    feat = [{'I':4, 'love':1, 'turnip':0}, {'I':1, 'love':0, 'turnip':9}, {'I':2, 'love':4, 'turnip':6}]
+    lab = ['yes', 'yes', 'no']
+    model.train(feat, lab)
 
 
 
